f941c4dbe3f3c2821b378b8889fd1bb09cb72a0d
Steve Forde <sforde08@gmail.com>
2026-07-08 01:38:56 +0100

    test: simulate broken health check

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ const PORT = process.env.PORT || 5001
 
 app.use(express.static('dist'))
 
+app.get('/health', (req, res) => {
+  // eslint-disable-next-line no-constant-condition
+  if (true) throw('error...  ')
+  res.send('ok')
+})
+
 const start = async () => {
   await app.listen(PORT)
 
